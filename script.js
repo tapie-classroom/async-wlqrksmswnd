962f59b43422897c0df624b532aab3b6de573823
wlqrksmswnd <wlqrksmswnd@taipe.kr>
2025-05-07 17:38:55 +0900

    날씨

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("fsdfg")
+fetch('https://api.openweathermap.org/data/2.5/weather?lat=37.53609444&lon=126.9675222&appid=1678d5fbe5655c5df4baca85f98cc3ed')
+    .then(json => {
+        const temp = json.main.temp;
+        const wind = json.main.wind;
+    })
