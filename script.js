66dba8397f4cf74d7309b0598c391bcbf046b6bf
wlqrksmswnd <wlqrksmswnd@taipe.kr>
2025-05-07 22:36:16 +0900

    온도 습도 기온 표시 기능 및 아이콘 표시 기능

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-fetch('https://api.openweathermap.org/data/2.5/weather?lat=37.53609444&lon=126.9675222&appid=1678d5fbe5655c5df4baca85f98cc3ed')
-    .then(json => {
-        const temp = json.main.temp;
-        const wind = json.main.wind;
+fetch('https://api.openweathermap.org/data/2.5/weather?lat=37.53609444&lon=126.9675222&appid=1678d5fbe5655c5df4baca85f98cc3ed&units=metric')
+    .then(response => {
+        return response.json()})
+    .then(data => {
+        let temp = data["main"]["temp"];
+        let humidity = data["main"]["humidity"];
+        let wind = data["wind"]["speed"];
+        let icon = data["weather"][0]['icon']
+        console.log("https://openweathermap.org/img/wn/" + icon + "@2x.png");
+        let tempurature = document.getElementById("temp");
+        tempurature.textContent = "날씨 검색 중..";
+        setTimeout( ()=> {
+            let water = document.getElementById("water");
+            let win = document.getElementById("win");
+            tempurature.textContent = "온도 : " + temp + "°C";
+            water.textContent = "습도 : " + humidity + "%";   
+            win.textContent = "바람 : " + wind + "m/s";
+            const weathericon = document.getElementById("weathericon");
+            weathericon.src = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+        },3000);
     })
+    
